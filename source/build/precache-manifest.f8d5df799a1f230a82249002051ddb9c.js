06d7626dea4178bc443ecce33c99ec67291f714c
radhey.baps <radhey.baps@gmail.com>
2020-07-31 14:41:38 -0400

    blog commit

---
+++ source/build/precache-manifest.f8d5df799a1f230a82249002051ddb9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca49a3aca362d4fa77eef3ae0375bbe8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb855c67dfac1c2332a8",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "13a55942bd85c8fb852b",
+    "url": "/static/js/2.fc7156f9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.fc7156f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb855c67dfac1c2332a8",
+    "url": "/static/js/main.a0e080bb.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
